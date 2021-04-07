a794dd993ab758b8bd3addcbdcccd7ca0354e043
Jared Nolen <Legodoghead@gmail.com>
2021-04-07 14:18:28 -0700

    Added some rectangles but they dont work

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    Scene: [Menu, Play],
 }
 
 let game = new Phaser.Game();
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
