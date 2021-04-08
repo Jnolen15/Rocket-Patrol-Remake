bd4232e5751ea008dc4216cbadea3398154d8556
Jared Nolen <Legodoghead@gmail.com>
2021-04-08 16:57:28 -0700

    Imported assets / made starfield background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,14 +5,26 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload(){
+        this.load.image('starfield', 'assets/starfield.png')
+    }
+
     create() {
+        // Starfield bacground
+        this.starfield = this.add.tileSprite(0,0,640,480, 'starfield').setOrigin(0,0);
+        
         // Green UI Background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00,).setOrigin(0,0);
-        // White border
+        // White borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-
+        
     }
+
+    update(){
+        this.starfield.tilePositionX -= 4;
+    }
+    
 }
