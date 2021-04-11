4f3c5e59ed3830d317ca5201d5423fff47c4b4d8
Jared Nolen <Legodoghead@gmail.com>
2021-04-11 16:27:51 -0700

    added ./ in front of file loads

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,11 +6,11 @@ class Play extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('starfield', 'assets/starfield.png');
-        this.load.image('rocket', 'assets/rocket.png');
-        this.load.image('spaceship', 'assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
         //load spritesheet
-        this.load.spritesheet('explosion', 'assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
         }
 
     create() {
